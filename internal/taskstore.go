69522446f3657c30c85c3aa75a37cc8c5f4e6061
Rinat Shaihetdinov <rinat.shaihetdinov@simbirsoft.com>
2021-12-26 17:49:28 +0500

    taskstore чужой

---
+++ internal/taskstore.go
@@ -0,0 +1,158 @@
+// package taskstore provides a simple in-memory "data store" for tasks.
+// Tasks are uniquely identified by numeric IDs.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package taskstore
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Task struct {
+	Id   int       `json:"id"`
+	Text string    `json:"text"`
+	Tags []string  `json:"tags"`
+	Due  time.Time `json:"due"`
+}
+
+// TaskStore is a simple in-memory database of tasks; TaskStore methods are
+// safe to call concurrently.
+// sync.Mutex - мьютекс
+
+/* 
+пример из руководства 
+
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+лок c.mu.Lock()
+инлок c.mu.Unlock()
+ */
+
+type TaskStore struct {
+	sync.Mutex  
+
+	tasks  map[int]Task
+	nextId int
+}
+
+func New() *TaskStore {
+	ts := &TaskStore{}
+	ts.tasks = make(map[int]Task)  // создание среза
+	ts.nextId = 0
+	return ts
+}
+
+// CreateTask creates a new task in the store.
+func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
+	//
+	ts.Lock()  // лок стора
+	defer ts.Unlock() // после выполнения фукции инлок 
+
+	task := Task{
+		Id:   ts.nextId,
+		Text: text,
+		Due:  due}
+	task.Tags = make([]string, len(tags)) // срез длиной количество тегов
+	copy(task.Tags, tags)
+
+	ts.tasks[ts.nextId] = task
+	ts.nextId++
+	return task.Id
+}
+
+// GetTask retrieves a task from the store, by id. If no such id exists, an
+// error is returned.
+func (ts *TaskStore) GetTask(id int) (Task, error) {
+	ts.Lock()
+	defer ts.Unlock()
+
+	t, ok := ts.tasks[id]
+	if ok {
+		return t, nil
+	} else {
+		return Task{}, fmt.Errorf("task with id=%d not found", id)
+	}
+}
+
+// DeleteTask deletes the task with the given id. If no such id exists, an error
+// is returned.
+func (ts *TaskStore) DeleteTask(id int) error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	if _, ok := ts.tasks[id]; !ok {
+		return fmt.Errorf("task with id=%d not found", id)
+	}
+
+	delete(ts.tasks, id)
+	return nil
+}
+
+// DeleteAllTasks deletes all tasks in the store.
+func (ts *TaskStore) DeleteAllTasks() error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	ts.tasks = make(map[int]Task)
+	return nil
+}
+
+// GetAllTasks returns all the tasks in the store, in arbitrary order.
+func (ts *TaskStore) GetAllTasks() []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+
+	/* 
+	возвращаю копию массива потому что ?????
+	 */
+	allTasks := make([]Task, 0, len(ts.tasks))  //  длину (0) и вместимость (количество тасков)
+	for _, task := range ts.tasks {
+		allTasks = append(allTasks, task)
+	}
+	return allTasks
+}
+
+// GetTasksByTag returns all the tasks that have the given tag, in arbitrary
+// order.
+func (ts *TaskStore) GetTasksByTag(tag string) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+taskloop:
+	for _, task := range ts.tasks {
+		for _, taskTag := range task.Tags {
+			if taskTag == tag {
+				tasks = append(tasks, task)
+				continue taskloop
+			}
+		}
+	}
+	return tasks
+}
+
+// GetTasksByDueDate returns all the tasks that have the given due date, in
+// arbitrary order.
+func (ts *TaskStore) GetTasksByDueDate(year int, month time.Month, day int) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+	for _, task := range ts.tasks {
+		y, m, d := task.Due.Date()
+		if y == year && m == month && d == day {
+			tasks = append(tasks, task)
+		}
+	}
+
+	return tasks
+}
